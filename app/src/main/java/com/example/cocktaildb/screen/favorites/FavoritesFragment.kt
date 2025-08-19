405e6ffbfe293ea5909282705a8c4823db9612c2
Do Quang Tuan <quangtuan9102003@gmail.com>
2025-08-19 23:45:27 +0700

    feat: offline profile + favorite list

--- app/src/main/java/com/example/cocktaildb/screen/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/cocktaildb/screen/favorites/FavoritesFragment.kt
@@ -1,10 +1,8 @@
 package com.example.cocktaildb.screen.favorites
 
 import android.graphics.Rect
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
